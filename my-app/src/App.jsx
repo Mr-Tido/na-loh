ecc75874caf0b0b8428fa2b3c1c41022e656190a
asdasd <asdasd@asdasd.ru>
2024-03-06 18:28:00 +0200

    asd

---
+++ my-app/src/App.jsx
@@ -0,0 +1,25 @@
+function App(){
+    return(
+        <>
+        <header>
+            <nav>
+                <div>
+                    <a href="">About</a>
+                    <a href="">Manifesto</a>
+                    <a href="">Projects</a>
+                </div>
+                <div>
+                    <a href="">Shop</a>
+                    <a href="">Contact</a>
+                    <a href="">It</a>
+                    <a href="">Cart (0)</a>
+                </div>
+            </nav>
+            <div className="h1_container">
+                <h1>marta/benet</h1>
+            </div>
+        </header>
+        </>
+    )
+}
+export default App
